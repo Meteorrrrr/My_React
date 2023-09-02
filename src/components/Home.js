6b2039a834384f425ba7c27d06df5062679e429b
tranthach <tranthach3132@gmail.com>
2023-09-02 15:36:15 +0700

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import styles from'./Global.module.css'
 import clsx from 'clsx'
 function Home() {
     return (
-        <h1 className={clsx(styles.colorRed)}>Home</h1>
+        <h1 className={clsx(styles.colorRed)}>Home1</h1>
     )
 }
 export default Home
